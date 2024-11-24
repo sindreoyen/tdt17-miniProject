a37ce1b3e72d8014d7cb38d08d84fb9eb9f4eacb
sindreoyen <sindreoyen@hotmail.com>
2024-11-24 18:20:52 +0100

    feat: data augmentation

--- src/train_model.py
+++ src/train_model.py
@@ -4,12 +4,25 @@ from ultralytics import YOLO
 
 print("Training model...")
 # Path to model with pre-trained weights
-__pretrained_model = os.path.join("..", "models", "pre-trained", "yolo11s.pt") # COCO pre-trained model
-__data = os.path.join("train", "data.yaml") # Data file
+__pretrained_model = os.path.join("models", "pre-trained", "yolo11s.pt") # COCO pre-trained model
 
 # Train the model
 pretrained = YOLO(__pretrained_model)
-result = pretrained.train(data=__data, 
-                          epochs=20, 
-                          imgsz=1024, 
-                          device=[0, 1])
+result = pretrained.train(
+    data="data.yaml", 
+    epochs=100, 
+    imgsz=1024, 
+    device=[0, 1], 
+    show_labels=False, 
+    patience=10, 
+    # Augmentation
+    degrees=2.0,          # Slight rotation
+    scale=0.4,            # Slight scaling
+    translate=0.1,        # Translation in image plane
+    shear=2.0,            # Slight shearing
+    mosaic=1.0,           # Mosaic augmentation
+    hsv_h=0.015,          # Slight hue adjustment
+    hsv_s=0.5,            # Moderate saturation adjustment
+    hsv_v=0.4,            # Brightness adjustment
+    label_smoothing=0.05, # Slight label smoothing
+)
